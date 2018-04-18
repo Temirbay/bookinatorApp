df5a7746aae4788ff0498338ac50375132fa5729
Temirbay <temirbaymiras.ktl@gmail.com>
2018-04-19 03:50:49 +0600

    menu updated

--- front-end/src/components/Menu.js
+++ front-end/src/components/Menu.js
@@ -14,17 +14,91 @@ class Menu extends React.Component {
 
   constructor(props) {
     super (props);
+
+    this.state = {
+      classNameHome: 'menu-bar-element-active',
+      classNameCatalog: 'menu-bar-element',
+      classNameMyBooks: 'menu-bar-element',
+      classNameDiscussions: 'menu-bar-element',
+    }
   }
+
+  handleClickHome = () => {
+      this.setState({
+        classNameHome: 'menu-bar-element-active',
+        classNameCatalog: 'menu-bar-element',
+        classNameMyBooks: 'menu-bar-element',
+        classNameDiscussions: 'menu-bar-element',
+      });
+  }
+
+  handleClickCatalog = () => {
+    this.setState({
+      classNameHome: 'menu-bar-element',
+      classNameCatalog: 'menu-bar-element-active',
+      classNameMyBooks: 'menu-bar-element',
+      classNameDiscussions: 'menu-bar-element',
+    });
+}
+
+handleClickMyBooks = () => {
+  this.setState({
+    classNameHome: 'menu-bar-element',
+    classNameCatalog: 'menu-bar-element',
+    classNameMyBooks: 'menu-bar-element-active',
+    classNameDiscussions: 'menu-bar-element',
+  });
+}
+
+handleClickDiscussions = () => {
+  this.setState({
+    classNameHome: 'menu-bar-element',
+    classNameCatalog: 'menu-bar-element',
+    classNameMyBooks: 'menu-bar-element',
+    classNameDiscussions: 'menu-bar-element-active',
+  });
+}
 
 
   render() {
     return (
       <div className="menu">
        <ul className="menu-bar">
-        <li><NavLink className="menu-bar-element" to={'/'}>Home</NavLink></li>
-        <li><NavLink className="menu-bar-element" to={'/catalog'}>Catalog</NavLink></li>
-        <li><NavLink className="menu-bar-element" to={'/mybooks'}>My Books</NavLink></li>
-        <li><NavLink className="menu-bar-element" to={'/discussions'}>Discussions</NavLink></li>
+        <li>
+          <NavLink 
+            className={this.state.classNameHome}
+            to={'/'}
+            onClick={this.handleClickHome}>
+              Home
+            </NavLink>
+        </li>
+        
+        <li>
+          <NavLink 
+            className={this.state.classNameCatalog} 
+            to={'/catalog'}
+            onClick={this.handleClickCatalog}>
+              Catalog
+            </NavLink>
+        </li>
+        
+        <li>
+          <NavLink 
+            className={this.state.classNameMyBooks} 
+            to={'/mybooks'}
+            onClick={this.handleClickMyBooks}>
+              My Books
+            </NavLink>
+        </li>
+
+        <li>
+          <NavLink 
+            className={this.state.classNameDiscussions} 
+            to={'/discussions'}
+            onClick={this.handleClickDiscussions}>
+              Discussions
+            </NavLink>
+        </li>
       </ul>
       </div>
     );
